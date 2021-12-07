7e8717035ca3ba3aea28201bb6702349482ffb5f
saibhushan37 <saibhushan37@gmail.com>
2021-12-07 10:48:31 +0530

    changes made in variable.tf file

--- variable.tf
+++ variable.tf
@@ -2,4 +2,7 @@ variable "subnetcidr1" {}
 variable "subnetcidr2" {}
 variable "subnetcidr3" {}
 variable "vpccidr" {}
-    
+variable "instancesize" {}  
+variable "enable_public_ip" {} 
+variable "instancename" {}
+variable "amiid" {}
