8a173fba023454f00d86fa495129add1ef9db160
saibhushan37 <saibhushan37@gmail.com>
2021-12-07 11:07:05 +0530

    final otput of instance

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,9 @@ resource "aws_instance" "Terraform-ec2" {
     ami = var.amiid
     instance_type = var.instancesize
     subnet_id = aws_subnet.Subnet-1.id
-    count = var.instancecount
     associate_public_ip_address = var.enable_public_ip
     availability_zone = "us-east-2a"
     tags = {
-      Name = var.instancename[count.index]
+      Name = var.instancename
+    }
+}
